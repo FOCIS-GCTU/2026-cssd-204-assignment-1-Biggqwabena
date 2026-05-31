6aab224c7cea40d3866ae7b7d50888f3bd507e04
Biggqwabena <2425402467@live.gctu.edu.gh>
2026-05-31 21:19:38 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Nathaniel Osei Asante
+# STUDENT ID: 2425402467
+# Email: 2425402467@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Nathaniel Osei Asante, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This main function prints my initials in large stylized letters.
+    print()
+    print("...NOA")
+    print()
+    print("...N..........N.......OOOOOOOOOO.......AAAA........")
+    print("...NN.........N......OOOOOOOOOOOO.....AA..AA.......")
+    print("...N.N........N......OO........OO....AA....AA......")
+    print("...N..N.......N......OO........OO...AA......AA.....")
+    print("...N...N......N......OO........OO..AA........AA....")
+    print("...N....N.....N......OO........OO..AAAAAAAAAAAA....")
+    print("...N.....N....N......OO........OO..AA........AA....")
+    print("...N......N...N......OO........OO..AA........AA....")
+    print("...N.......N..N.**...OOOOOOOOOOOO..AA........AA.**")
+    print("...N........NNN.**....OOOOOOOOOO...AA........AA.**")
+    print()
 
 
 main()
